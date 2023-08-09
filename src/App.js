e757cbcb28df897907262c09b27528dfb718a189
Savan Kumar <savansharma22@gmail.com>
2023-08-09 20:05:25 +0530

    progress bar with timmer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [start, setStart] = useState(10);
+  const [bar, setBar] = useState(0);
+  useEffect(() => {
+    if (start > 0)
+      setTimeout(async () => {
+        await setStart(start - 1);
+        setBar(11 - start);
+      }, 1000);
+  }, [start]);
+
+  console.log(start);
+  console.log(bar);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{start}</div>
+      <progress width="100%" value={bar} max={10}></progress>
     </div>
   );
 }
